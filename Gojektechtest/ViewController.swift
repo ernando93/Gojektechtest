da38fcbe618569e9f43e44931bf0aec467faa1fb
Ernando Kasaluhe <ernandokasaluhe@Ernandos-MacBook-Pro.local>
2019-09-14 19:50:53 +0700

    First Commit Gojek Tech Test

---
+++ Gojektechtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gojektechtest
+//
+//  Created by Ernando Kasaluhe on 14/09/19.
+//  Copyright © 2019 Ernando Kasaluhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
